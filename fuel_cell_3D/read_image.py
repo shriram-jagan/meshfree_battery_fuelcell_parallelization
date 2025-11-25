f9b1cf63b1e9599318522126989d7c8ff1845c18
xinhe2205 <45882470+xinhe2205@users.noreply.github.com>
2025-11-25 15:24:58 -0700

    3D code

---
+++ fuel_cell_3D/read_image.py
@@ -0,0 +1,64 @@
+import time
+start_time = time.time()
+import numpy as np
+from numpy import sign
+
+import matplotlib.pyplot as plt
+
+from tqdm import tqdm
+
+from numba import jit
+
+from scipy.sparse import csc_matrix, csr_matrix, bmat
+from scipy.sparse.linalg import spsolve
+from scipy.sparse.linalg import eigs
+
+from numpy.linalg import norm, eig
+
+from collections import Counter
+
+import tifffile
+
+
+#########################
+# read image in
+#########################
+
+def read_in_image(file_name, studied_physics, dimention):
+
+    img_ = tifffile.imread(file_name) # np array
+
+    grain_id_counter = Counter(img_.flatten())
+    unic_grain_id = [] # save the unique grain IDs. For fuel cell, 0: pore, 1: electrolyte, 2: electrode.
+
+    for key in grain_id_counter:
+        unic_grain_id.append(int(key))
+        
+    num_pixels_xyz = [] # number of volxels in x y z directions.
+
+    num_pixels_x = np.shape(img_)[0]
+    num_pixels_y = np.shape(img_)[1]
+
+    num_pixels_xyz.append(num_pixels_x)
+    num_pixels_xyz.append(num_pixels_y)
+
+    if dimention == 3:
+        num_pixels_z = np.shape(img_)[2]
+        num_pixels_xyz.append(num_pixels_z)
+
+
+    return img_, unic_grain_id, num_pixels_xyz
+
+
+ 
+
+
+
+
+             
+            
+
+
+
+
+
